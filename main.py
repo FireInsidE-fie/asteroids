8abc39083cce848bc78eaaa5866bbf871bbd4467
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-01-30 19:22:54 +0100

    use sprite groups

--- main.py
+++ main.py
@@ -14,15 +14,20 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-        player.update(dt)
+        updatable.update(dt)
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
